54b68d6c40e6a89d1567b036859a027b46ef5f67
Imanbekov007 <osmonimanbekov.ksl@gmail.com>
2022-10-24 20:23:00 +0600

    hello

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int san= random.nextInt(1,5);
+        System.out.println(san);
+        System.out.println(method(scanner.nextInt(), san));
+        System.out.println("hello");
+    }
+
+    public static boolean method(int number,int number1) {
+        if(number==number1){
+            return true;
+        }else {
+            return false;
+        }
+
+
+    }
+}
